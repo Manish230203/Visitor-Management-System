08ef6cc1453d6ea4244a647f389ddbdb18b9b7d1
Kunal Malekar <152507658+Kunal1203Malekar@users.noreply.github.com>
2026-04-02 16:06:31 +0530

    Add files via upload

---
+++ VMS_Backend/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "vms_db",
+    "user": "postgres",
+    "password": "1234",
+    "port": 5432
+}
+
+SECRET_KEY = "vms_secret_key_2026"
